5f3b0b2f903cb8eeaf44cb418c3bd2c6150568c2
ee <sunlixin@eedemacbook-air.local>
2016-09-02 15:42:27 +0800

    用于测试

--- LXPageMenu/LXPageMenu/lxTestViewController.h
+++ LXPageMenu/LXPageMenu/lxTestViewController.h
@@ -6,6 +6,9 @@
 //  Copyright © 2016年 ee. All rights reserved.
 //
 
+/**
+ *  用于测试
+ */
 #import <UIKit/UIKit.h>
 
 @interface lxTestViewController : UIViewController
--- LXPageMenu/LXPageMenu/lxTestViewController.m
+++ LXPageMenu/LXPageMenu/lxTestViewController.m
@@ -6,6 +6,10 @@
 //  Copyright © 2016年 ee. All rights reserved.
 //
 
+
+/**
+ *  用于测试
+ */
 #import "lxTestViewController.h"
 
 @interface lxTestViewController ()
@@ -24,14 +28,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
